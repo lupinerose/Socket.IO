09225b36efee1cb23c88b5b123687ef7816b0b1c
Brooke Kullberg <brookekullberg@gmail.com>
2019-08-21 12:18:50 -0700

    add line to emit message

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    // console.log('message: ' + msg);
+    io.emit('chat messssage', msg);
   });
 });
 
